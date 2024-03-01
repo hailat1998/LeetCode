1e6f0dfce913545d7b27248ebcf5dfa66dbde7df
hailat1998 <htemesgen400@gmail.com>
2024-03-01 19:29:50 +0000

    solved

--- Le#977.kt
+++ Le#977.kt
@@ -1,19 +1,5 @@
 class Solution {
-    fun findJudge(n: Int, t: Array<IntArray>): Int {
-        val arr1 = IntArray(n + 1) { 0 }
-        val arr2 = IntArray(n + 1) { 0 }
-
-        for (i in t) {
-            arr1[i[0]]++
-            arr2[i[1]]++
-        }
-
-        for (i in 1..n) {
-            if (arr1[i] == 0 && arr2[i] == n - 1) {
-                return i
-            }
-        }
-
-        return -1
+    fun sortedSquares(nums: IntArray): IntArray {
+        return nums.map{ it*it}.sorted().toIntArray()
     }
 }
